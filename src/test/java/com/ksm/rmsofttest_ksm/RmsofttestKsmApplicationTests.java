dec0f544abf74be85dd3b54e7d3b845e41284d84
ksm <ksmles36@imokorea.co.kr>
2023-12-20 23:49:36 +0900

    first commit

---
+++ src/test/java/com/ksm/rmsofttest_ksm/RmsofttestKsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksm.rmsofttest_ksm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmsofttestKsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
